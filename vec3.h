446eddc68ad7aaf8af8e06d0182be2cd2a074dde
Shang <zhaoxiaoshang@gmail.com>
2014-10-09 10:24:15 -0400

    Create vec3.h

---
+++ vec3.h
@@ -0,0 +1,52 @@
+#ifndef __GWU_VEC__
+#define __GWU_VEC__
+
+#include <assert.h>
+//================================
+// vec3
+//================================
+class vec3 {
+public :
+	float x, y, z;
+
+public :
+	vec3();
+	vec3( float x, float y, float z );
+
+	vec3 &			set( float x, float y, float z );
+
+	vec3 &			zero( void );
+	
+	vec3			operator- ( void ) const;
+	vec3			operator+ ( void ) const;
+
+	vec3			operator+ ( const vec3 &v ) const;
+	vec3			operator- ( const vec3 &v ) const;
+	vec3			operator* ( float scalar ) const;
+	vec3			operator/ ( float scalar ) const;
+	
+	vec3 &			operator= ( const vec3 &v );
+	vec3 &			operator+=( const vec3 &v );
+	vec3 &			operator-=( const vec3 &v );	
+	vec3 &			operator*=( float scalar );
+	vec3 &			operator/=( float scalar );
+
+	float &			operator[]( int index );
+	const float &	operator[]( int index ) const;
+
+	float			dot( const vec3 &v ) const;
+	vec3           cross(const vec3 &v) const; /// adding cross
+
+	float			magnitude( void ) const;
+	vec3 &			normalize( void );
+
+	float *			ptr( void );
+	const float *	ptr( void ) const;
+
+public :
+	friend vec3		operator*( float scalar, const vec3 &v );
+};
+
+
+
+#endif // __GWU_VEC__
